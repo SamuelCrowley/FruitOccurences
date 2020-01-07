8d31d07de74ff3b57595138834e33134275fc4e7
SamuelCrowley <59050177+SamuelCrowley@users.noreply.github.com>
2020-01-07 18:56:02 +0000

    Add files via upload

---
+++ FruitOccurences/FruitOccurences/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FruitOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = new string[10];
+            List<string> fruitStrings = new List<string>();
+            Random random = new Random();
+
+            fruits[0] = "Apple";
+            fruits[1] = "Banana";
+            fruits[2] = "Kiwi";
+            fruits[3] = "Apricot";
+            fruits[4] = "Blueberry";
+            fruits[5] = "Cherry";
+            fruits[6] = "Grapefruit";
+            fruits[7] = "Guava";
+            fruits[8] = "Fig";
+            fruits[9] = "Mango";
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                int count = random.Next(1, 30);
+                for (int j = 0; j < count; j++)
+                {
+                    fruitStrings.Add(fruits[i]);
+                }
+            }
+
+            Console.WriteLine("The fruit that occured the most frequently was " + FindMostFrequentFruit(fruitStrings));
+        }
+
+        static string FindMostFrequentFruit(List<string> fruitStrings)
+        {
+            Dictionary<string, int> fruitDictionary = new Dictionary<string, int>();
+
+            foreach (var item in fruitStrings)
+            {
+                if (fruitDictionary.ContainsKey(item))
+                {
+                    fruitDictionary[item] += 1;
+                }
+                else
+                {
+                    fruitDictionary.Add(item, 1);
+                }
+            }
+
+            int highest = 0;
+            string returnString = "";
+            string times = "";
+            foreach (var item in fruitDictionary)
+            {
+                times = item.Value == 1 ? " time" : " times";
+                Console.WriteLine(item.Key + " appeared " + item.Value + times);
+                if (item.Value > highest)
+                {
+                    highest = item.Value;
+                    returnString = item.Key;
+                }
+            }
+            return returnString;
+        }
+    }
+}
